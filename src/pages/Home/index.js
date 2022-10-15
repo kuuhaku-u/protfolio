66fc3d45e8cd18c6082ec6711169e407acd66153
kuuhaku-u <ryusama6996@gmail.com>
2022-10-15 12:33:48 +0530

    added navbar

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavBar } from '../../components/navigation/navbar'
+
+export const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            Home
+        </div>
+    )
+}
